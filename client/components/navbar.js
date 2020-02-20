62fb6a42f33ce3647df93eca1024efe74fd6aba3
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2020-02-20 12:17:52 -0500

    changed header and title, no biggies

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import { logout } from '../store';
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
-    <h1>STOCK PORTFOLIO</h1>
+    <h1>ANDY'S EZ STOCKS</h1>
     <nav>
       {isLoggedIn ? (
         <div>
