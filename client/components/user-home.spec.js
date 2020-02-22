d9705504d7c54491c0ad451b7fa7268565bbe6db
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2020-02-22 10:28:03 -0500

    CI issue: changing the welcome message caused a built-in test to fail, so I changed the test to match expected output

--- client/components/user-home.spec.js
+++ client/components/user-home.spec.js
@@ -1,10 +1,10 @@
 /* global describe beforeEach it */
 
-import {expect} from 'chai';
+import { expect } from 'chai';
 import React from 'react';
-import enzyme, {shallow} from 'enzyme';
+import enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import {UserHome} from './user-home';
+import { UserHome } from './user-home';
 
 const adapter = new Adapter();
 enzyme.configure({adapter});
@@ -13,10 +13,10 @@ describe('UserHome', () => {
   let userHome;
 
   beforeEach(() => {
-    userHome = shallow(<UserHome email="cody@email.com" />);
+    userHome = shallow(<UserHome username="codyThePug" />);
   });
 
   it('renders the email in an h3', () => {
-    expect(userHome.find('h3').text()).to.be.equal('Welcome, cody@email.com');
+    expect(userHome.find('h3').text()).to.be.equal('Welcome, codyThePug');
   });
 });
