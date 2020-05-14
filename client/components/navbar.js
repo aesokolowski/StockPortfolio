cbafe4f84e9990c5163b65570178d3ce2672987d
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2020-05-13 21:56:18 -0400

    feature: navbar buttons show which link button represents the currently active link

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,32 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { logout } from '../store';
 
-const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div className="navbar">
-    <h1 className="titletext">ANDY'S EZ STOCKS</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div className="menubuttons">
-          {/* The navbar will show these links after you log in */}
-          <Link className="button" to="/portfolio">Portfolio</Link>
-          <Link className="button" to="/transactions">Transactions</Link>
-          <a className="button" href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div className="menubuttons">
-          {/* The navbar will show these links before you log in */}
-          <Link className="button" to="/login">Login</Link>
-          <Link className="button" to="/register">Register</Link>
-        </div>
-      )}
-    </nav>
-  </div>
-);
+const Navbar = ({ handleClick, isLoggedIn }) => {
+
+  return (
+    <div className="navbar">
+      <h1 className="titletext">ANDY'S EZ STOCKS</h1>
+      <nav>
+        {isLoggedIn ? (
+          <div className="menubuttons">
+            {/* The navbar will show these links after you log in */}
+            <NavLink
+              className="button"
+              activeClassName="active"
+              to="/portfolio"
+            >
+              Portfolio
+            </NavLink>
+            <NavLink className="button" to="/transactions">Transactions</NavLink>
+            <a className="button" href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div className="menubuttons">
+            {/* The navbar will show these links before you log in */}
+            <NavLink
+              className="button"
+              activeClassName="active"
+              to="/login"
+            >
+              Login
+            </NavLink>
+            <NavLink
+              className="button"
+              to="/register"
+            >
+              Register
+            </NavLink>
+          </div>
+        )}
+      </nav>
+    </div>
+  );
+};
 
 /**
  * CONTAINER
